8f71a16b8ba3e1842c1bb59c0a846cd76cc2fed9
Elostay <anastasiia.motsukh@automaze.io>
2024-03-24 23:00:51 +0200

    _

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,39 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true,
-//     serverComponentsExternalPackages: ["mongoose"],
-//   },
-//   images: {
-//     domains: ["lh3.googleusercontent.com"],
-//   },
-//   webpack(config) {
-//     config.experiments = {
-//       ...config.experiments,
-//       topLevelAwait: true,
-//     };
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   appDir: true,
-  //   serverComponentsExternalPackages: ["mongoose"],
-  // },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-
-  // webpack(config) {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true,
-  //   };
-  //   return config;
-  // },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
+
+// const nextConfig = {
+//   // experimental: {
+//   //   appDir: true,
+//   //   serverComponentsExternalPackages: ["mongoose"],
+//   // },
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+
+//   // webpack(config) {
+//   //   config.experiments = {
+//   //     ...config.experiments,
+//   //     topLevelAwait: true,
+//   //   };
+//   //   return config;
+//   // },
+// };
+
+// export default nextConfig;
